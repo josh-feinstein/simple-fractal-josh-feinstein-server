a8474cb725a76fd274b36e119c206042fe798629
Josh Feinstein <pillboxmediainc@gmail.com>
2019-02-19 22:45:56 -0500

    changes test get route url

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = app.listen(PORT, () => console.log('Connected to port ' + PORT));
 
-app.get('/test', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.json({ test: 123 });
 });
