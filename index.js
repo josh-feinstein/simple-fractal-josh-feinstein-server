9504057fba9762ac52eede46d6e4b39f7dd51909
Josh Feinstein <pillboxmediainc@gmail.com>
2019-02-19 23:01:49 -0500

    adds callback to papaparse

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const Papa = require('papaparse');
 
 app.listen(PORT, () => console.log('Connected to port ' + PORT));
 
-app.get('/', (req, res, next) => {
-  res.send('test');
+app.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  const scoreRecords = Papa.parse(
+    'https://s3.amazonaws.com/simple-fractal-recruiting/score-records.csv',
+    {
+      header: true,
+      download: true,
+      complete: function(results) {
+        res.send(results);
+      },
+    }
+  );
+
+  //   Papa.parse('https://s3.amazonaws.com/simple-fractal-recruiting/companies.csv', {
+  //     header: true,
+  //     download: true,
+  //     complete: this.updateCompaniesData,
+  //   });
 });
