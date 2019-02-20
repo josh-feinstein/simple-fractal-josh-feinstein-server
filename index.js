71f1758e965a73b0d485660f6eb9873d6e6e8d65
Josh Feinstein <pillboxmediainc@gmail.com>
2019-02-19 22:48:32 -0500

    changes response from json to send

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const server = app.listen(PORT, () => console.log('Connected to port ' + PORT));
+
+app.listen(PORT, () => console.log('Connected to port ' + PORT));
 
 app.get('/', (req, res, next) => {
-  res.json({ test: 123 });
+  res.send('test');
 });
